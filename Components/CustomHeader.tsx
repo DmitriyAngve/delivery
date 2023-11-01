27e7e9fb9432580f704b6eae3b057564494e8b2d
DmitriyAngve <kirirch129@gmail.com>
2023-11-01 19:00:27 +0700

    feat: header done

--- Components/CustomHeader.tsx
+++ Components/CustomHeader.tsx
@@ -8,15 +8,61 @@ import {
   Image,
 } from "react-native";
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "../constants/Colors";
+import { Link } from "expo-router";
 
-function CustomHader() {
+const SearchBar = () => (
+  <View style={styles.searchContainer}>
+    <View style={styles.searchSection}>
+      <View style={styles.searchField}>
+        <Ionicons
+          style={styles.searchIcon}
+          name="ios-search"
+          size={20}
+          color={Colors.medium}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Restarauts, grociries, dishes"
+        />
+      </View>
+      <Link href={"/"} asChild>
+        <TouchableOpacity style={styles.optionButton}>
+          <Ionicons name="options-outline" size={20} color={Colors.primary} />
+        </TouchableOpacity>
+      </Link>
+    </View>
+  </View>
+);
+
+function CustomHeader() {
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
         <TouchableOpacity>
-          <Image source={require("../assets/images/bike.png")} />
+          <Image
+            style={styles.bike}
+            source={require("../assets/images/bike.png")}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.titleContainer}>
+          <Text style={styles.title}>Delivery - Now</Text>
+          <View style={styles.locationName}>
+            <Text style={styles.subtitle}>Moscow</Text>
+            <Ionicons name="chevron-down" size={20} color={Colors.primary} />
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.profileButton}>
+          <Ionicons
+            name="person-outline"
+            size={20}
+            color={Colors.primary}
+          ></Ionicons>
         </TouchableOpacity>
       </View>
+      <SearchBar />
     </SafeAreaView>
   );
 }
@@ -28,8 +74,66 @@ const styles = StyleSheet.create({
   },
   container: {
     height: 60,
-    backgroundColor: "red",
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    gap: 20,
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+  },
+  bike: {
+    height: 50,
+    width: 50,
+  },
+  titleContainer: {
+    flex: 1,
+  }, // сдвигает "Home" к иконке велосипедиста
+  title: {
+    fontSize: 14,
+    color: Colors.medium,
+  },
+  locationName: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  profileButton: {
+    backgroundColor: Colors.lightGrey,
+    padding: 10,
+    borderRadius: 50,
+  },
+  searchContainer: {
+    height: 60,
+    backgroundColor: "#fff",
+  },
+  searchSection: {
+    flexDirection: "row",
+    gap: 10,
+    flex: 1,
+    paddingHorizontal: 20,
+    alignItems: "center",
+  },
+  searchField: {
+    flex: 1,
+    backgroundColor: Colors.lightGrey,
+    borderRadius: 8,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  input: {
+    padding: 10,
+    color: Colors.mediumDark,
+  },
+  searchIcon: {
+    paddingLeft: 10,
+  },
+  optionButton: {
+    padding: 10,
+    borderRadius: 50,
   },
 });
 
-export default CustomHader;
+export default CustomHeader;
