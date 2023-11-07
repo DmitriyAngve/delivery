18ff60ea5575f9d3285eb3ff416ab7df498ebe70
DmitriyAngve <kirirch129@gmail.com>
2023-11-07 18:38:31 +0700

    #

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,10 @@ import { Stack } from "expo-router";
 import CustomHeader from "../Components/CustomHeader";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 
+import Colors from "../constants/Colors";
+import { TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: "index",
@@ -15,6 +19,27 @@ export default function RootLayoutNav() {
         <Stack.Screen
           name="index"
           options={{ header: () => <CustomHeader /> }}
+        />
+        {/* options={{ presentation: "modal" }} - делает роутинг на новую страницу в виде модалки */}
+        <Stack.Screen
+          name="(modal)/filter"
+          options={{
+            presentation: "modal",
+            headerTitle: "Filter",
+            headerShadowVisible: false,
+            headerStyle: {
+              backgroundColor: Colors.lightGrey,
+            },
+            headerLeft: () => (
+              <TouchableOpacity onPress={() => {}}>
+                <Ionicons
+                  name="close-outline"
+                  size={28}
+                  color={Colors.primary}
+                />
+              </TouchableOpacity>
+            ),
+          }}
         />
       </Stack>
     </BottomSheetModalProvider>
