c4da9d95efbfea1dbf1fa963020ded5de673a292
DmitriyAngve <kirirch129@gmail.com>
2023-11-11 19:38:40 +0700

    #

---
+++ Components/Restaurants.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Restaraunts = () => {
+  return (
+    <View>
+      <Text>Restaraunts</Text>
+    </View>
+  );
+};
+
+export default Restaraunts;
--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,34 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, StyleSheet } from "react-native";
 import React from "react";
+import Categories from "../Components/Categories";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Restaraunts from "../Components/Restaurants";
 
 const Page = () => {
-  return <View>{/* <Text>Page</Text> */}</View>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <Categories />
+        <Text style={styles.header}>Top picks in your neighbourhood</Text>
+        <Restaraunts />
+        <Text style={styles.header}>Top picks in your neighbourhood</Text>
+        <Restaraunts />
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    top: 30,
+    backgroundColor: "#fff",
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 16,
+    paddingHorizontal: 16,
+  },
+});
 
 export default Page;
