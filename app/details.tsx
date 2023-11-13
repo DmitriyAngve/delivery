ec36ff24306829cac4379225b9be05c744e39b2f
DmitriyAngve <kirirch129@gmail.com>
2023-11-13 19:09:21 +0700

    #

---
+++ app/details.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import ParallaxScrollView from "../Components/ParallaxScrollView";
+
+const Details = () => {
+  return (
+    <>
+      <ParallaxScrollView />
+    </>
+  );
+};
+
+export default Details;
